85a4488a4355d3741c60391fde1088a63e8d7f93
Lukasz Szczesniak <veiner87@gmail.com>
2017-05-03 14:20:44 +0200

    added white spaces

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
-var triangle1Area = getTriangleArea(10, 15);
+var triangle1Area = getTriangleArea(0, 15);
 var triangle2Area = getTriangleArea(20, 11);
 var triangle3Area = getTriangleArea(3, 6);
 
 function getTriangleArea(a, h) {
 	if ( (a <= 0) || (h <= 0) ) {
-		console.log('Nieprawidłowe dane!');
+		return ('Nieprawidłowe dane!');
 	}
-	return a*h/2;
+	return a * h / 2;
 }
 console.log( getTriangleArea(10,6) );
 console.log(triangle1Area);
