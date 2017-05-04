bca5ad82f517c09cfd82adad42c874980d503057
Lukasz Szczesniak <veiner87@gmail.com>
2017-05-04 11:49:49 +0200

    cleaning up mess

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,11 @@ var triangle3Area = getTriangleArea(3, 6);
 
 function getTriangleArea(a, h) {
 	if ( (a <= 0) || (h <= 0) ) {
-		return ('Nieprawidłowe dane!');
+		return 'Nieprawidłowe dane!';
 	}
 	return a * h / 2;
 }
-console.log( getTriangleArea(10,6) );
+console.log(getTriangleArea(10,6));
 console.log(triangle1Area);
 console.log(triangle2Area);
 console.log(triangle3Area);
